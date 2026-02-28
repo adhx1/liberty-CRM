40acaf39d1d2ae05b730c1b7ca8ce4da729d0c9f
MUHAMMAD ADHIL <muhammadadhil.personal@gmail.com>
2026-02-28 15:08:55 +0530

    Fix Sidebar import case issue

--- frontend/src/components/layouts/sidebar.jsx
+++ frontend/src/components/layouts/sidebar.jsx
@@ -1,7 +1,7 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import "./Sidebar.css";
+import "./sidebar.css";
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
   const { logout } = useContext(AuthContext);
