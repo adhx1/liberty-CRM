cf4e6f434fededbd603985106a95a04ddd9468c5
MUHAMMAD ADHIL <muhammadadhil.personal@gmail.com>
2026-03-01 15:28:21 +0530

    secure allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,3 +168,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'your_email@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_app_password'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
